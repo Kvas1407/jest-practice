9ea73f58a01a116074f5f64518bf43579568eeb8
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-04-20 18:46:42 -0400

    Added a dummy test, to check whether the test file is in a good state to run the test

---
+++ test/cookbook.test.js
@@ -0,0 +1,28 @@
+const { Cookbook } = require('../src/cookbook');
+
+describe('Cookbook', () => {
+  describe('Adding recipes', () => {
+    test('should allow a recipe to be added', () => {
+      expect(true). toBe(true);
+
+    });
+  });
+
+  describe('Listing recipes', () => {
+    test('should allow the recipes to be listed', () => {
+
+    });
+  });
+
+  describe('Retrieving a recipe', () => {
+    test('should allow the ingredients for a recipe to be retrieved', () => {
+
+    });
+  });
+
+  describe('Deleting a recipe', () => {
+    test('should allow a recipe to be deleted', () => {
+
+    });
+  });
+});
